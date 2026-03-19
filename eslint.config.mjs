44de12c867c808db7d57519dba5bb2997700bd88
Eid Mohammad Pouya <empouya03@gmail.com>
2026-03-19 12:30:41 +0100

    chore: initialize monorepo with pnpm workspace, tsconfig base, eslint, prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import prettier from "eslint-config-prettier";
+
+export default [
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            parser: tsparser,
+            parserOptions: { project: "./tsconfig.base.json" }
+        },
+        plugins: { "@typescript-eslint": tseslint },
+        rules: {
+            ...tseslint.configs.recommended.rules,
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }]
+        }
+    },
+    prettier
+];
